2aa7e4a0fe2cc9d8a7c48c32e09aebba5ba56e14
Chris Rimmer <c@24.org.uk>
2013-02-24 00:32:28 +0000

    Added multiples of ten

--- Numbers.hs
+++ Numbers.hs
@@ -4,11 +4,20 @@ module Numbers
 
 numberToText :: Int -> String
 numberToText x  
-    | x < 0 = "Minus " ++ numberToText (-x)
-    | otherwise = smallNumToText x
+    | x < 0 = "Minus " ++ posNumToText (-x)
+    | otherwise = posNumToText x
+
+posNumToText :: Int -> String
+posNumToText x
+    | x < 20 = smallNumToText x
+    | otherwise = sub100ToText x
 
 smallNumToText :: Int -> String
 smallNumToText x = ["Zero", "One", "Two", "Three", "Four", "Five", 
                   "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
                   "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
                   "Eighteen", "Nineteen"] !! x
+
+sub100ToText :: Int -> String
+sub100ToText x = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", 
+                 "Seventy", "Eighty", "Ninety"] !! (x `div` 10)
--- NumbersTest.hs
+++ NumbersTest.hs
@@ -6,11 +6,15 @@ conversionTest d w = TestCase (assertEqual describe w (numberToText d))
   where describe = "numberToText " ++ (show d) 
 
 tests = TestList [conversionTest d w | (d,w) <- [
+                    ((-50), "Minus Fifty"),
                     ((-14), "Minus Fourteen"),
                     ((-8), "Minus Eight"),
                     (0,"Zero"), 
                     (1, "One"), 
                     (2, "Two"), 
                     (6, "Six"), 
-                    (13, "Thirteen")
+                    (13, "Thirteen"),
+                    (20, "Twenty"),
+                    (30, "Thirty"),
+                    (70, "Seventy")
                  ]]
